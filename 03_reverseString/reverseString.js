0ad3bb7bf8d3cc385ebae15f14e73dd6a98e2afd
Kathode <dtu@deakin.edu.au>
2024-07-21 00:18:37 +1000

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  const stringArray = [];
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringArray.push(string[i]);
+  }
+
+  return stringArray.join("");
 };
 
 // Do not edit below this line
