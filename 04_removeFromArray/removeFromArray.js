f7165936a3b11f897aaa7052b842cfd227a82293
Kathode <dtu@deakin.edu.au>
2024-07-21 00:49:00 +1000

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...valuesToRemove) {
+  return array.filter((currentValue) => {
+    return !valuesToRemove.some((removeValue) => removeValue === currentValue);
+  });
 };
 
 // Do not edit below this line
