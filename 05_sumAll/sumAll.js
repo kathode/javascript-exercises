1aa69b6164bf8dcc5c48ae4d344543efd17ba907
Kathode <dtu@deakin.edu.au>
2024-07-21 01:12:28 +1000

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (firstRange, secondRange) {
+  let sum = 0;
 
+  if (firstRange < 0 || secondRange < 0) return "ERROR";
+  if (!Number.isInteger(firstRange) || !Number.isInteger(secondRange)) return "ERROR";
+
+  if (firstRange < secondRange) {
+    for (let i = firstRange; i <= secondRange; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = secondRange; i <= firstRange; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
