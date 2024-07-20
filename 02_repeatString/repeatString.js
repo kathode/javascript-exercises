3beff9a0a492c27d90bce9d9cd50de1427d3302d
Kathode <dtu@deakin.edu.au>
2024-07-21 00:00:33 +1000

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, repeats) {
+  let output = "";
 
+  if (repeats === 0) {
+    return "";
+  }
+
+  if (repeats < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < repeats; i++) {
+    output += string;
+  }
+
+  return output;
 };
 
 // Do not edit below this line
