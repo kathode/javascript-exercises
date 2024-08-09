38888af38f8992a8c1a4274eda890fa0c73a863e
Kathode <dtu@deakin.edu.au>
2024-08-10 02:13:36 +1000

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let stringArray = string.replace(/[\W_]/g, "").toLowerCase().split("");
 
+  for (let i = stringArray.length; i > 0; i--) {
+    if (stringArray[i - 1] !== stringArray[stringArray.length - i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
