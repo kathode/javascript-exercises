ed64911e1c6775fcd7c6bc130c3e978ab928a5bf
Kathode <dtu@deakin.edu.au>
2024-08-10 01:23:47 +1000

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (values) {
+  return values.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (values) {
+  return values.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (value) {
+  let j = 1;
+  for (let i = value; i > 0; i--) {
+    j = i * j;
+  }
+
+  return j;
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
